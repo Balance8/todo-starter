d3a4b06d738d7b85d262f4d266f3ffa57cbd2cbb
Balance8 <mslocum819@gmail.com>
2021-08-30 11:31:18 -0700

    todo-starter tutorial

---
+++ libs/feature-todos/src/lib/feature-todos.tsx
@@ -0,0 +1,14 @@
+import './feature-todos.module.scss';
+
+/* eslint-disable-next-line */
+export interface FeatureTodosProps {}
+
+export function FeatureTodos(props: FeatureTodosProps) {
+  return (
+    <div>
+      <h1>Welcome to FeatureTodos!</h1>
+    </div>
+  );
+}
+
+export default FeatureTodos;
---
+++ libs/feature-todos/src/lib/todo-item-list/todo-item-list.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useTodoItemsQuery } from '@todo-starter/data-access';
+
+/* eslint-disable-next-line */
+export interface TodoItemListProps {}
+
+export function TodoItemList(props: TodoItemListProps) {
+  const { loading, error, data } = useTodoItemsQuery();
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return (
+    <ul>
+      {data?.todoItems.map(({ id, text, done }) => (
+        <li key={id}>
+          {text} - <strong>{done ? 'Done' : 'Not Done'}</strong>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default TodoItemList;
